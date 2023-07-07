921b71b5d0b9e5d6a7241f1e27404134bef99798
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-07-07 20:41:39 +0700

    ref: update babel.config

--- shared/ui-component/babel.config.js
+++ shared/ui-component/babel.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-react",
+      "@babel/preset-env",
       {
-        targets: {
-          node: "current",
-        },
-      },
+        modules: false
+      }
     ],
+    "@babel/preset-react",
   ],
 };
